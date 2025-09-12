4783a6e1102706f7fa71492f6ed496035e5295e0
ret724 <126372788+ret724@users.noreply.github.com>
2025-09-12 11:30:05 -0500

    code yay!

--- FlatList_L8_L9/app/index.tsx
+++ FlatList_L8_L9/app/index.tsx
@@ -1,14 +1,29 @@
-import {
-  Text,
-  View,
-  StyleSheet,
-  FlatList,
-  TouchableOpacity,
-} from "react-native";
 import colors from "@/styles/colors";
 import defaultStyles from "@/styles/defaultStyles";
+import { useState } from "react";
+import { FlatList, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
+
+  type dataType = {
+    id: string;
+    title: string;
+  }
+
+  const DATA: dataType[] = [
+    {id: "1", title: "First Item"},
+    {id: "2", title: "Second Item"},
+    {id: "3", title: "Third Item"},
+    {id: "4", title: "Fourth Item"},
+  ];
+
+  const [selectedId, setSelectedId] = useState<string>("1");
+
+  const itemSelected = (item: dataType) => {
+    console.log(item.title);
+    setSelectedId(item.id);
+  }
+
   return (
     <View style={defaultStyles.container}>
       <View style={defaultStyles.titleContainer}>
@@ -16,7 +31,17 @@ export default function Index() {
       </View>
       <View style={[defaultStyles.textContainer, { flex: 1 }]}>
         <View style={styles.flatlist}>
-          <Text>This is where our list will go</Text>
+          <FlatList
+            data = {DATA}
+            keyExtractor={(item: dataType) => item.id}
+            renderItem={({item}) => (
+              <TouchableOpacity onPress={() => itemSelected(item)}>
+                <View style={[styles.flatListRow, {backgroundColor: item.id === selectedId ? colors.primary : colors.secondary}]}>
+                  <Text style={[styles.titleText, {color: item.id === selectedId ? colors.text.light : colors.text.dark}]}>{item.title}</Text>
+                </View>
+              </TouchableOpacity>
+            )}
+          />
         </View>
       </View>
     </View>
@@ -37,4 +62,12 @@ const styles = StyleSheet.create({
     fontSize: 24,
     padding: 10,
   },
+  flatListRow: {
+    // backgroundColor: "lightgreen",
+    margin: 5,
+    padding: 5,
+    width: 120,
+    justifyContent: "center",
+    alignItems: "center",
+  }
 });
